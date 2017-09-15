bb283337abcb6f1206f437ab7ee7ca44febdb8ed
aiamateur <rashmiravinathan@gmail.com>
2017-09-15 08:56:27 +0530

    Update week4_lec19_donts_generate_blocks.v

--- week4_lec19_donts_generate_blocks.v
+++ week4_lec19_donts_generate_blocks.v
@@ -115,5 +115,36 @@ module RCA(carry_out, sum, a, b, carry_in);
            end
        endgenerate
 endmodule
+       
+//Final Compiled Version
+       
+module RCA(carry_out, sum, a, b, carry_in);
+       parameter N = 8;
+       input [N-1:0] a, b;
+       input carry_in;         //input carry_in
+       
+       output [N-1:0] sum;
+       output carry_out;       //output carry_out
+       
+       wire [N:0] carry;      //carry[N] is carry out
+       
+       assign carry[0] = carry_in;
+       assign carry_out = carry[N];
+       
+       genvar i;
+       
+       generate
+         for (i = 0; i < N; i=i+1)
+           begin: fa_loop
+             //wire t1, t2, t3;
+             //xor G1 (t1, a[i], b[i]);
+             //xor G2 (sum[i], t1, carry[i]);       
+             //and G3 (t2, a[i], b[i]);
+             //and G4 (t3, t1, carry[i]);
+             //or  G5 (carry[i+1], t2, t3);
+             full_adder FA (a[i], b[i], carry[i], sum[i], carry[i+1]);
+           end
+       endgenerate
+endmodule
            
            

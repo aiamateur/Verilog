fd8b50c88b201edb2f131cf48a6b783b0b851c90
aiamateur <rashmiravinathan@gmail.com>
2017-09-23 20:23:32 +0530

    Update week5_lec24_modeling_fsm2.v

--- week5_lec24_modeling_fsm2.v
+++ week5_lec24_modeling_fsm2.v
@@ -85,6 +85,70 @@ module parity_gen2(x, clk, z);
 endmodule
 //This design will not cause the synthesis tool to generate a latch for the output "z"
 
+//Example 3: Sequence detector for pattern "0110"
+//This is not compiling
+
+module seq_detector(x, clk, reset, z);
+  input x, clk, reset;
+  output reg z;
+
+  parameter S0 = 0; S1 = 1; S2 = 2; S3 = 3;
+  reg [0:1] PS, NS;
+  
+  always @(posedge clk or posedge reset)
+    if(reset) PS <= S0;
+    else      PS <= NS;
+    
+  always @(PS, x)
+    case (PS)
+      S0 : begin
+             z  = x ? 0 : 0;
+             NS = x ? S0 : S1;
+           end
+      S1 : begin
+             z  = x ? 0 : 0;
+             NS = x ? S2 : S1;
+           end 
+      S2 : begin
+             z  = x ? 0 : 0;
+             NS = x ? S3 : S1;
+           end
+      S3 : begin
+             z  = x ? 0 : 1;
+             NS = x ? S0 : S1;
+           end
+    endcase
+endmodule
+
+module test_sequence;
+  reg x, clk, reset;
+  wire z;
+  
+  seq_detector SEQ(x, clk, reset, z);
+  
+  initial
+    begin
+      $dumpfile("sequence.vcd");
+      $dumpvars(0, test_sequence);
+      clk = 1'b0; reset = 1'b1;
+      #15 reset = 1'b0; 
+    end
+    
+  always
+    #5 clk = ~clk;
+    
+  initial
+    begin
+      #12 x = 0; #10 x = 0; #10 x = 1 ; #10 x = 1;
+      #10 x = 0; #10 x = 1; #10 x = 1 ; #10 x = 0;
+      #10 x = 0; #10 x = 1; #10 x = 1 ; #10 x = 0;
+      #10 $finish;
+    end
+    
+endmodule
+  
+
+
 
 
   
